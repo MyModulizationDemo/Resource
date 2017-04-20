af5b99b1644d4e4642a328fb443a7a28552428bb
JasonZhang <JasonZhang@JasonZhangdeMacBook-Pro.local>
2017-04-20 14:26:25 +0800

    first commit

---
+++ MyResource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyResource
+//
+//  Created by JasonZhang on 2017/4/20.
+//  Copyright © 2017年 JasonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyResource/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyResource
+//
+//  Created by JasonZhang on 2017/4/20.
+//  Copyright © 2017年 JasonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
